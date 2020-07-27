c53e914a900c3ee7d513f0932b1b764ed302878e
Jacob Eckert <jweckert17@gmail.com>
2020-07-27 13:55:03 -0500

    filters now stack - i.e. filtering by officer and crime will give you a list of criminals arrested by officer who were convicted of crime

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -6,25 +6,41 @@ import { Criminal } from './Criminal.js';
 const domNode = document.querySelector('.criminalsContainer');
 const eventHub = document.querySelector('.container');
 
+// keeps track of which filters are currently active under key of type of filter
+// i.e. - activeFilters.conviction = <function to filter criminal array by selected conviction>
+const activeFilters = {};
+
 eventHub.addEventListener('convictionChanged', event => {
   const convictions = useConvictions();
   const conviction = convictions.find(conviction => conviction.id === event.detail.convictionId);
 
-  const criminals = useCriminals();
-  const filteredCriminals = criminals.filter(criminal => criminal.conviction === conviction.name);
+  activeFilters.conviction = criminal => criminal.conviction === conviction.name;
 
-  render(filteredCriminals);
+  render(getFilteredCriminals());
 });
 
 eventHub.addEventListener('officerChanged', event => {
   const officers = useOfficers();
   const officer = officers.find(officer => officer.id === event.detail.officerId);
 
-  const criminals = useCriminals();
-  const filteredCriminals = criminals.filter(criminal => criminal.arrestingOfficer === officer.name);
+  activeFilters.officer = criminal => criminal.arrestingOfficer === officer.name;
 
-  render(filteredCriminals);
+  render(getFilteredCriminals());
 });
+
+/**
+ * Filter down the criminals array based on all filters currently active.
+ * Returns array of criminals after having been filtered by all active filters.
+ */
+const getFilteredCriminals = () => {
+  let filteredCriminals = useCriminals();
+
+  Object.keys(activeFilters).forEach(filterType => 
+    filteredCriminals = filteredCriminals.filter(activeFilters[filterType])
+  );
+
+  return filteredCriminals;
+};
 
 const render = criminals => {
   const criminalsHTML = criminals.map(Criminal).join('');
