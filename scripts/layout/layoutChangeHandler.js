b55d26b52cec75e52fe732f8c718aa00ff44cb0c
Jacob Eckert <jweckert17@gmail.com>
2020-08-03 13:15:34 -0500

    hide filters when witness list is displayed, add some documentation

--- scripts/layout/layoutChangeHandler.js
+++ scripts/layout/layoutChangeHandler.js
@@ -1,27 +1,39 @@
-const eventHub = document.querySelector('.container');
-const contentTarget = document.querySelector('.content--right');
-
+/**
+ * Module to listen for large-scale DOM modifications - eg showing or hiding the right column of the page, or determining which list should be rendered in the mainListContainer DOM node
+ */
 import { CriminalList } from '../criminals/CriminalList.js';
 import { WitnessList } from '../witnesses/WitnessList.js';
 
+const eventHub = document.querySelector('.container');
+const rightColumnDOMNode = document.querySelector('.content--right');
+const filtersDOMNode = document.querySelector('.filters');
+
+/**
+ * Listen for the notes toggled event, hide or show the right column of the page depending on hide or show selection
+ */
 eventHub.addEventListener('notesToggled', event => {
   const { shouldHideNotes } = event.detail;
 
   if(shouldHideNotes) {
-    contentTarget.classList.add('hide');
+    rightColumnDOMNode.classList.add('hide');
   }
   else {
-    contentTarget.classList.remove('hide');
+    rightColumnDOMNode.classList.remove('hide');
   }
 });
 
+/**
+ * Listen for the criminals or witness list toggle event - render the proper list and hide/show the filters depending on which list chosen
+ */
 eventHub.addEventListener('criminalsOrWitnessesToggled', event => {
   const listToDisplay = event.detail.listToDisplay;
 
   if(listToDisplay === 'criminalList') {
     CriminalList(); 
+    filtersDOMNode.classList.remove('hide');
   }
   else if(listToDisplay === 'witnessList') {
     WitnessList();
+    filtersDOMNode.classList.add('hide');
   }
 });
