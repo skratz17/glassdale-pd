1064c5b4cbe3b0c6d850847d42ec93319c7c5fb7
Jacob Eckert <jweckert17@gmail.com>
2020-08-13 10:50:36 -0500

    implemented form validation and error rendering on noteform

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -10,33 +10,38 @@ const eventHub = document.querySelector('.container');
  */
 contentTarget.addEventListener('submit', event => {
   if(event.target.id === 'note-form') {
+    const formElement = event.target;
+
     event.preventDefault();
 
-    const note = createNoteObjectFromFormData(event.target.elements);
+    const note = createNoteObjectFromFormData(formElement.elements);
 
     const errors = validator.validate(note);
+    renderErrors(errors);
 
     if(errors.length === 0) {
+      disableForm(formElement);
       saveNote(note);
     }
   }
 });
-
-
 
 const render = criminals => {
   contentTarget.innerHTML = `
     <h2 class="header">Create a New Note</h2>
     <form id="note-form">
       <div class="form-group">
+        <ul class="note-form__errors note-form__title-errors"></ul>
         <label class="note-form__label" for="title">Title</label>
         <input required class="note-form__input" type="text" name="title" id="note--title" placeholder="Enter a title">
       </div>
       <div class="form-group">
+        <ul class="note-form__errors note-form__author-errors"></ul>
         <label class="note-form__label" for="author">Author</label>
         <input required class="note-form__input" type="text" name="author" id="note--author" placeholder="Enter an author">
       </div>
       <div class="form-group">
+        <ul class="note-form__errors note-form__criminalId-errors"></ul>
         <label class="note-form__label" for="criminalId">Criminal</label>
         <select class="note-form__criminalId" id="note--criminalId" name="criminalId">
           <option value="0">Select a criminal...</option>
@@ -44,6 +49,7 @@ const render = criminals => {
         </select>
       </div>
       <div class="form-group">
+        <ul class="note-form__errors note-form__text-errors"></ul>
         <label class="note-form__label" for="text">Note Text</label>
         <textarea required class="note-form__input note-form__textarea" name="text" id="note--text" placeholder="Enter your note content"></textarea>
       </div>
@@ -75,8 +81,6 @@ const createNoteObjectFromFormData = formElements => {
   };
 
   for(const element of formElements) {
-    element.disabled = true;
-
     if(element.nodeName.toLowerCase() !== 'button') {
       let valueToStore = element.value;
 
@@ -89,6 +93,26 @@ const createNoteObjectFromFormData = formElements => {
   }
 
   return note;
+}
+
+const renderErrors = errors => {
+  clearErrors();
+  errors.forEach(error => {
+    const domNode = document.querySelector(`.note-form__${error.propertyName}-errors`);
+    domNode.innerHTML += `<li class="error">${error.errorMessage}</li>`
+  });
+}
+
+const clearErrors = () => {
+  document
+    .querySelectorAll('.note-form__errors')
+    .forEach(domNode => domNode.innerHTML = '');
+}
+
+const disableForm = formElements => {
+  for(const element of formElements) {
+    element.disabled = true;
+  }
 }
 
 // re-render the form once the note you submitted has been succesfully saved. this will clear all form inputs and re-enable all of them.
